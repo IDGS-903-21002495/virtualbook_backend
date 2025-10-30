ef726b2af11556ff3a501d4b670db7f7374ce3a5
IDGS-803-22002335 <tovardiazjuanaesmeralda@gmail.com>
2025-10-30 09:47:15 -0600

    VIR-18-CrudLibros

---
+++ virtualbook_backend/Dtos/LibroUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace virtualbook_backend.Dtos
+{
+    public class LibroUpdateDto
+    {
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Genero { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
